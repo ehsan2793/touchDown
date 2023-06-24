8f0636b7edbcd42866deed4163a642450e7bd518
ehsan2793 <ehsan2793@gmail.com>
2023-06-24 14:36:09 -0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Ehsan Rahimi on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
