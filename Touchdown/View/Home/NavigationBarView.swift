20c33d31be28e06161938c047dc043856a3090ad
ehsan2793 <ehsan2793@gmail.com>
2023-06-24 16:44:00 -0700

    added navbar to app

--- Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct NavigationBarView: View {
     // MARK: - PROPERTIES
 
+    @State private var isAnimated: Bool = false
+
     // MARK: - BODY
 
     var body: some View {
@@ -20,9 +22,16 @@ struct NavigationBarView: View {
                     .foregroundColor(.black)
             }) //: BUTTON
             Spacer()
-            
+
             LogoView()
-            
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+
             Spacer()
 
             Button(action: {}, label: {
