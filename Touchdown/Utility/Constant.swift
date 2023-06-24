cfc50875e0f46108a6b38710d30268189696f78b
ehsan2793 <ehsan2793@gmail.com>
2023-06-24 16:04:41 -0700

    first commit

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,27 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Ehsan Rahimi on 6/24/23.
+//
+
+import SwiftUI
+// DATA
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
