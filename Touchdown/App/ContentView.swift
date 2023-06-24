aa3185ed944adb2a9220e8912b0c92f37b285e9e
ehsan2793 <ehsan2793@gmail.com>
2023-06-24 16:15:53 -0700

    footer added

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -8,17 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: -  PROPERTIES
+
+    // MARK: - BODY
+
     var body: some View {
-        VStack {
-            Text("hello")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+       FooterView()
     }
 }
+
+// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
