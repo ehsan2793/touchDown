612d99938036c2af86782e4c948372c6c0255730
ehsan2793 <ehsan2793@gmail.com>
2023-06-24 21:39:41 -0700

    player struct added

---
+++ Touchdown/Extention/CodableBundleExtention.swift
@@ -0,0 +1,8 @@
+//
+//  CodableBundleExtention.swift
+//  Touchdown
+//
+//  Created by Ehsan Rahimi on 6/24/23.
+//
+
+import Foundation
---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Ehsan Rahimi on 6/24/23.
+//
+
+import Foundation
+
+struct Player: Hashable ,Codable , Identifiable {
+    var id: Int
+    var image: String
+}
