39c12c27e195fc354cac4157503afffe2853b83f
ehsan2793 <ehsan2793@gmail.com>
2023-06-25 19:29:43 -0700

    addd condition

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -13,39 +13,45 @@ struct ContentView: View {
     // MARK: - BODY
 
     var body: some View {
-        VStack(spacing: 0) {
-            NavigationBarView()
-                .padding(.horizontal, 15)
-                .padding(.bottom)
-                .background(Color.white)
-                .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0.0, y: 5)
-
-            ScrollView(.vertical, showsIndicators: false, content: {
+        VStack {
+            if shop.showingProduct == false && shop.selectedProduct == nil {
                 VStack(spacing: 0) {
-                    FeatuedTabView()
-                        .frame(height: 270, alignment: .top)
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0.0, y: 5)
 
-                    CategoryGridView()
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0) {
+                        FeatuedTabView()
+                            .frame(height: 270, alignment: .top)
 
-                    TitleView(title: "Helmets")
+                        CategoryGridView()
 
-                    LazyVGrid(columns: gridLayout, spacing: 15) {
-                        ForEach(products) { product in
-                            ProductItemView(product: product)
-                        } //: LOOP
-                    } //: VGRID
-                    .padding(15)
-                    
-                    TitleView(title: "Brands")
-                    
-                    BrandGridView()
+                        TitleView(title: "Helmets")
 
-                    FooterView()
-                        .padding(.horizontal)
-                } //: VSTACK
-            }) //: SCROLL
-        } //: VSTACK
-        .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+                        LazyVGrid(columns: gridLayout, spacing: 15) {
+                            ForEach(products) { product in
+                                ProductItemView(product: product)
+                            } //: LOOP
+                        } //: VGRID
+                        .padding(15)
+                        
+                        TitleView(title: "Brands")
+                        
+                        BrandGridView()
+
+                        FooterView()
+                            .padding(.horizontal)
+                    } //: VSTACK
+                }) //: SCROLL
+            } //: VSTACK
+                .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+            } else {
+                ProductDetailView()
+            }
+        }
     }
 }
 
@@ -54,5 +60,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(Shop())
     }
 }
