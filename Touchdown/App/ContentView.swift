1e3c4b52e021ce5a41f3ccf8409718647b3598c5
ehsan2793 <ehsan2793@gmail.com>
2023-06-25 19:35:15 -0700

    fix back button

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -9,44 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: -  PROPERTIES
+
     @EnvironmentObject var shop: Shop
+
     // MARK: - BODY
 
     var body: some View {
         VStack {
             if shop.showingProduct == false && shop.selectedProduct == nil {
                 VStack(spacing: 0) {
-                NavigationBarView()
-                    .padding(.horizontal, 15)
-                    .padding(.bottom)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0.0, y: 5)
+                    NavigationBarView()
+                        .padding(.horizontal, 15)
+                        .padding(.bottom)
+                        .background(Color.white)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0.0, y: 5)
 
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    VStack(spacing: 0) {
-                        FeatuedTabView()
-                            .frame(height: 270, alignment: .top)
+                    ScrollView(.vertical, showsIndicators: false, content: {
+                        VStack(spacing: 0) {
+                            FeatuedTabView()
+                                .frame(height: 270, alignment: .top)
 
-                        CategoryGridView()
+                            CategoryGridView()
 
-                        TitleView(title: "Helmets")
+                            TitleView(title: "Helmets")
 
-                        LazyVGrid(columns: gridLayout, spacing: 15) {
-                            ForEach(products) { product in
-                                ProductItemView(product: product)
-                            } //: LOOP
-                        } //: VGRID
-                        .padding(15)
-                        
-                        TitleView(title: "Brands")
-                        
-                        BrandGridView()
+                            LazyVGrid(columns: gridLayout, spacing: 15) {
+                                ForEach(products) { product in
+                                    ProductItemView(product: product)
+                                        .onTapGesture {
+                                            withAnimation(.easeOut) {
+                                                shop.selectedProduct = product
+                                                shop.showingProduct = true
+                                            }
+                                        }
+                                } //: LOOP
+                            } //: VGRID
+                            .padding(15)
 
-                        FooterView()
-                            .padding(.horizontal)
-                    } //: VSTACK
-                }) //: SCROLL
-            } //: VSTACK
+                            TitleView(title: "Brands")
+
+                            BrandGridView()
+
+                            FooterView()
+                                .padding(.horizontal)
+                        } //: VSTACK
+                    }) //: SCROLL
+                } //: VSTACK
                 .background(colorBackground.ignoresSafeArea(.all, edges: .all))
             } else {
                 ProductDetailView()
